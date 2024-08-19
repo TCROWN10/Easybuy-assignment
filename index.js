fe9db3aec83a651e8f1fc27398ce7a0c8dc98fb7
Bolaji oluwatobi <bolajioluwatobi249@gmail.com>
2024-08-19 14:13:41 +0100

    third commit

---
+++ index.js
@@ -0,0 +1,10 @@
+
+function showSidebar(){
+    const navigation = document.querySelector('.navigation')
+    navigation.style.display = 'flex'
+}
+
+function hideSidebar(){
+    const navigation = document.querySelector('.navigation')
+    navigation.style.display = 'none'
+}
